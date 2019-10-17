01226301f0e4402d1fe78a50741cde478967f360
ethanharrison <harrem18@wfu.edu>
2019-10-17 16:28:21 -0400

    Fill in Deck.h with the big 3 and required functions

--- Deck.h
+++ Deck.h
@@ -6,9 +6,35 @@
 class Deck {
 
 private:
-    Card* cards;
-    int arraySize;
-    int cardsLeft;
+    Card* cards; //pointer to array of cards
+    int arraySize; //physical size of the array
+    int cardsLeft; //logical size of the array
+
+public:
+    //Default Constructor for initializing the game
+    Deck();
+
+    //The Big Three
+
+    //1. Destructor
+    ~Deck();
+    //2. Copy constructor
+    Deck(const Deck& copyDeck);
+    //3. Copy assignment operator
+    Deck& operator = (Deck& deckToCopy);
+
+
+    //Populates the deck with 52 cards
+    void newDeck();
+
+    //Shuffles the cards in the deck
+    void shuffleDeck();
+
+    //Chooses a card from the deck
+    int chooseCard();
+
+    //Adds a card to the deck
+    bool addCard();
 };
 
 
